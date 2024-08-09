23fd5e1a1a5be28dc863be5953df45fda735ebde
RUHUTA🏀🙏 <RUHUTA@RUHUTA>
2024-08-10 00:27:28 +0200

    commit

---
+++ cookie_22rp10247/src/main/java/cookie_22rp10247/Ruhuta.java
@@ -0,0 +1,73 @@
+package cookie_22rp10247;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ruhuta
+ */
+@WebServlet("/Ruhuta")
+public class Ruhuta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ruhuta() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		// TODO Auto-generated method stub
+				//response.getWriter().append("Served at: ").append(request.getContextPath());
+		Cookie[] cookies = request.getCookies();
+        String username = "";
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("username".equals(cookie.getName())) {
+                    username = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        request.setAttribute("username", username);
+        request.getRequestDispatcher("ish.jsp").forward(request, response);
+			}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		// TODO Auto-generated method stub
+				String username = request.getParameter("username");
+
+		        // Create a cookie to store the username
+		        Cookie userCookie = new Cookie("username", username);
+		        userCookie.setMaxAge(60*60*24*30); // 30 days
+		        response.addCookie(userCookie);
+
+		        // Redirect to the JSP page
+		        response.sendRedirect("ish.jsp");
+
+	}
+
+}
